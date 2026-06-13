f188575123e8c4b82da26877b070b9ed5732ffcc
Manas27 <manasgharat24@gmail.com>
2026-06-13 10:15:49 +0530

    Add files via upload

---
+++ Even-Odd.c
@@ -0,0 +1,18 @@
+//Manas Gharat
+//2503033111372031
+
+#include <stdio.h>
+int main() {
+
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+    printf("Number is Even\n");
+    } else {
+    printf("Number is Odd\n");
+    }
+    return 0;
+}   
