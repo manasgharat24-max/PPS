2d5f13f3faad0b6f99c6b1ac4006c23a0a6a1a12
Manas27 <manasgharat24@gmail.com>
2026-06-13 10:13:47 +0530

    Add files via upload

---
+++ Star4.c
@@ -0,0 +1,21 @@
+//Manas Gharat
+//PRN: 2503033111372031
+
+#include<stdio.h>
+int main() {
+
+    int i, j, k;
+    for(i = 1; i <= 3; i++) {
+        for(k = 1; k <i; k++) {
+            printf("\t");
+        }
+
+        for(j=i; j <= 3; j++) {
+            printf("*\t");
+        }
+
+        printf("\n");
+
+    }
+    return 0;
+}   
